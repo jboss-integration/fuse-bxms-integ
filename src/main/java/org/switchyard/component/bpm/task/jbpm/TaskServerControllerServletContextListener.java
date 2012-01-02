8271f0dbbb4148ff4f4b8e9e8c6cbe299e907c38
David Ward <david@errantepiphany.com>
2012-01-02 14:00:09 -0500

    SWITCHYARD-515: Upgrade Drools from 5.2.1 to 5.3.1 and jBPM from 5.1.1 to 5.2.0

--- src/main/java/org/switchyard/component/bpm/task/jbpm/TaskServerControllerServletContextListener.java
+++ src/main/java/org/switchyard/component/bpm/task/jbpm/TaskServerControllerServletContextListener.java
@@ -16,52 +16,39 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
  * MA  02110-1301, USA.
  */
-package org.switchyard.component.bpm.common;
+package org.switchyard.component.bpm.task.jbpm;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 /**
- * Represents available process actions.
+ * TaskServerControllerServletContextListener.
  *
  * @author David Ward &lt;<a href="mailto:dward@jboss.org">dward@jboss.org</a>&gt; (C) 2011 Red Hat Inc.
  */
-public enum ProcessActionType {
+public class TaskServerControllerServletContextListener implements ServletContextListener {
 
-    /** startProcess . */
-    START_PROCESS(ProcessConstants.START_PROCESS),
-    /** signalEvent . */
-    SIGNAL_EVENT(ProcessConstants.SIGNAL_EVENT),
-    /** abortProcessInstance . */
-    ABORT_PROCESS_INSTANCE(ProcessConstants.ABORT_PROCESS_INSTANCE);
-
-    private final String _action;
+    private TaskServerController _controller = null;
 
     /**
-     * Constructs a new ProcessActionType with the specified action.
-     * @param action the action
+     * {@inheritDoc}
      */
-    ProcessActionType(String action) {
-        _action = action;
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        _controller = new TaskServerController();
+        _controller.start();
     }
 
     /**
-     * Gets the action.
-     * @return the action
+     * {@inheritDoc}
      */
-    public String action() {
-        return _action;
-    }
-
-    /**
-     * Gets the ProcessActionType matching the specified action.
-     * @param action the action
-     * @return the matching ProcessActionType
-     */
-    public static ProcessActionType fromAction(String action) {
-        for (ProcessActionType pat : values()) {
-            if (pat.action().equals(action)) {
-                return pat;
-            }
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        try {
+            _controller.stop();
+        } finally {
+            _controller = null;
         }
-        return null;
     }
 
 }
