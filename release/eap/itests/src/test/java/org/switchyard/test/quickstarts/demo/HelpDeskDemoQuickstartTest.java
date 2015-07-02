eb2183d599b765aae5f4e64bbcfed6d714dfc345
dvirgiln <david.virgil.naranjo@gmail.com>
2015-07-02 10:45:02 +0200

    ENTESB-3540  Comment the Helpdesk quickstart

--- release/eap/itests/src/test/java/org/switchyard/test/quickstarts/demo/HelpDeskDemoQuickstartTest.java
+++ release/eap/itests/src/test/java/org/switchyard/test/quickstarts/demo/HelpDeskDemoQuickstartTest.java
@@ -18,6 +18,7 @@ import junit.framework.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.switchyard.test.quickstarts.util.BRMSArquillianUtil;
@@ -28,6 +29,7 @@ import org.switchyard.test.quickstarts.util.BRMSArquillianUtil;
  * @author David Ward &lt;<a href="mailto:dward@jboss.org">dward@jboss.org</a>&gt; &copy; 2012 Red Hat Inc.
  */
 @RunWith(Arquillian.class)
+@Ignore
 public class HelpDeskDemoQuickstartTest {
 
     @Deployment(testable = false)
