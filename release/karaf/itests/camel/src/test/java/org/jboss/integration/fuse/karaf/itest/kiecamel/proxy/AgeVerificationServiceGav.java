4b2f63e87c3fb1d67f4b85fb5415dc696092735b
Jiri Petrlik <jiripetrlik@gmail.com>
2015-11-27 13:51:32 +0100

    New test scenario: Use Camel route to execute rules imported by GAV.

---
+++ release/karaf/itests/camel/src/test/java/org/jboss/integration/fuse/karaf/itest/kiecamel/proxy/AgeVerificationServiceGav.java
@@ -0,0 +1,8 @@
+package org.jboss.integration.fuse.karaf.itest.kiecamel.proxy;
+
+/**
+ * Created by jpetrlik on 11/11/15.
+ */
+public interface AgeVerificationServiceGav {
+    Object verifyAge(Object person);
+}
