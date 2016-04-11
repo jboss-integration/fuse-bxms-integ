39ad5517334a940dfe1e98c9db538fd0811ebcc5
dvirgiln <david.virgil.naranjo@gmail.com>
2016-04-11 09:34:31 +0200

    ENTESB-5295 Delete camel-module not required folders

--- release/karaf/itests/camel/src/test/java/org/jboss/integration/fuse/karaf/itest/kiecamel/proxy/AgeVerificationServiceGav.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.integration.fuse.karaf.itest.kiecamel.proxy;
-
-/**
- * Created by jpetrlik on 11/11/15.
- */
-public interface AgeVerificationServiceGav {
-    Object verifyAge(Object person);
-}
