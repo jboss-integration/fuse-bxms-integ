2dda7aa171acf79fea0b74998a9dfaac5cd4fb3a
Tom Cunningham <tcunning@redhat.com>
2016-06-10 16:20:41 -0400

    ENTESB-5394 Add installation instructions for "switchyard-rules-loading" quickstart in Readme.md

--- quickstarts/switchyard-rules-loading/Readme.md
+++ quickstarts/switchyard-rules-loading/Readme.md
@@ -8,9 +8,47 @@ The Junit test initially is ignored. It is an arquillian test that require an EA
 
 But this important feature of switchyard can be tested executing the /release/eap/itest. There is a specific test that deploys an artifact on the local maven repo, and later redeploy the same artifact (with different drools rules).
 
+
 Preqrequisites 
 ==============
 Maven
 EAP with Switchyard and Fuse-integration installed
+
+
+EAP
+----------
+If installers have been previously installed on EAP skips the steps 1-4
+
+1.  Download JBoss EAP 6.4 series
+
+2.  Install Fuse over your EAP instance:
+
+java -jar fuse-eap-installer.jar ${eap.home}
+
+3.  Install KIE over your EAP instance:
+
+java -jar kie-eap-installer.jar ${eap.home}
+
+4.  Install the fuse-integration:
+
+java -jar fuse-integration-eap-installer-${version.fuse.bxms-integration}.jar ${eap.home}
+
+5. Start EAP in standalone mode:
+
+${AS}/bin/standalone.sh
+
+6. Build and deploy the quickstart:
+
+mvn install -Pdeploy
+
+7. Submit a webservice request to invoke the Client<br/>
+```
+mvn exec:java
+```
+<br/>
+
+8. Undeploy the quickstart:
+
+mvn clean -Pdeploy
 
 
--- quickstarts/switchyard-rules-loading/src/test/java/org/switchyard/quickstarts/rules/loading/HttpClient.java
+++ quickstarts/switchyard-rules-loading/src/test/java/org/switchyard/quickstarts/rules/loading/HttpClient.java
@@ -13,74 +13,122 @@
  */
 package org.switchyard.quickstarts.rules.loading;
 
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import java.util.Date;
+
+import org.switchyard.component.test.mixins.http.HTTPMixIn;
+import com.google.common.io.CharStreams;
 
 /**
  * The Class HttpClient.
  */
 public class HttpClient {
 
-    /**
-     * Send.
-     *
-     * @param message the message
-     * @param url the url
-     * @param method the method
-     * @return the string
-     * @throws Exception the exception
-     */
-    public String send(String message, String url, String method) throws Exception {
-        URL location = new URL(url);
-        HttpURLConnection client = (HttpURLConnection) location.openConnection();
-        int respCode = -1;
-        client.setRequestMethod(method);
-        client.setDoInput(true);
+    /** The Constant HELLO_SERVICE_ADDRESS. */
+    private static final String HELLO_SERVICE_ADDRESS = "http://localhost:8080/hello-greeting-service";
 
-        int len = message.length();
-        client.setRequestProperty("Content-Length", Integer.toString(len));
+    /** The Constant CIAO_SERVICE_ADDRESS. */
+    private static final String CIAO_SERVICE_ADDRESS = "http://localhost:8080/ciao-greeting-service";
 
-        if (message.length() > 0) {
-            client.setDoOutput(true);
-        }
+    /** The Constant ORIGINAL_KIE_MODULE_NAME. */
+    private static final String ORIGINAL_KIE_MODULE_NAME = "original-project.jar";
 
-        client.connect();
+    /** The Constant MODIFIED_KIE_MODULE_NAME. */
+    private static final String MODIFIED_KIE_MODULE_NAME = "modified-project.jar";
 
-        if (message.length() > 0) {
-            OutputStreamWriter out = new OutputStreamWriter(client.getOutputStream());
-            out.write(message, 0, len);
-            out.flush();
+    /** The Constant KIE_SCANNER_TEST. */
+    private static final String KIE_SCANNER_TEST = "switchyard-rules-loading";
 
-            client.getOutputStream().close();
-        }
+    static {
+        // assemble a Kie module
+        ShrinkWrap.create(JavaArchive.class).addAsResource("HelloGreetingService.drl").addAsResource("CiaoGreetingService.drl")
+                .addAsManifestResource("kmodule.xml").as(ZipExporter.class)
+                .exportTo(new File("target", ORIGINAL_KIE_MODULE_NAME), true);
 
-        respCode = client.getResponseCode();
-
-        InputStream response = null;
-        if (respCode < 400) {
-            response = client.getInputStream();
-        } else {
-            response = client.getErrorStream();
-        }
-
-        StringBuilder sb = new StringBuilder();
-        String line;
-        BufferedReader rd = new BufferedReader(new InputStreamReader(response));
-
-        while ((line = rd.readLine()) != null) {
-            sb.append(line + '\n');
-        }
-
-        response.close();
-        rd.close();
-        client.disconnect();
-
-        return sb.toString();
-
+        // assemble a modified Kie module
+        ShrinkWrap.create(JavaArchive.class).addAsResource("HelloGreetingService-modified.drl")
+                .addAsResource("CiaoGreetingService-modified.drl").addAsManifestResource("kmodule.xml").as(ZipExporter.class)
+                .exportTo(new File("target", MODIFIED_KIE_MODULE_NAME), true);
     }
+
+    /**
+     * Touch module.
+     *
+     * @param name the name
+     */
+    private static void touchModule(String name) {
+        File module = new File("target", name);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        module.setLastModified(new Date().getTime());
+    }
+
+    /**
+     * Install module.
+     *
+     * @param name the name
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    private static void installModule(String name) throws IOException {
+        System.out.println("Installing " + name);
+        // it is necessary to touch a module to give a Kie container to know
+        // that it is updated
+        touchModule(name);
+        final Process process = Runtime.getRuntime().exec(
+                String.format("mvn install:install-file -Dfile=target/%s -DpomFile=src/main/resources/pom.xml", name));
+        // read stdout/stderr
+        String out = CharStreams.toString(new InputStreamReader(process.getInputStream()));
+        String err = CharStreams.toString(new InputStreamReader(process.getErrorStream()));
+        System.out.println(out);
+        System.out.println(err);
+    }
+
+    public static void main (String ignored[]) {
+        try { 
+            installModule(ORIGINAL_KIE_MODULE_NAME);
+            System.out.println("Waiting 10s for deploy...");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        final HTTPMixIn http = new HTTPMixIn();
+        http.initialize();
+        String response = http.sendString(HELLO_SERVICE_ADDRESS, "first", HTTPMixIn.HTTP_POST);
+        System.out.println("Response should equal 'Hello first!' : " + response);
+        response = http.sendString(CIAO_SERVICE_ADDRESS, "second", HTTPMixIn.HTTP_POST);
+        System.out.println("Response should equal 'Ciao second!' : " + response);
+
+        try {
+            installModule(MODIFIED_KIE_MODULE_NAME);
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        response = http.sendString(HELLO_SERVICE_ADDRESS, "first", HTTPMixIn.HTTP_POST);
+        System.out.println("Response should equal 'Modified Hello first!' : " + response);
+        response = http.sendString(CIAO_SERVICE_ADDRESS, "second", HTTPMixIn.HTTP_POST);
+        System.out.println("Response should equal 'Modified Ciao second!' : " + response);
+    }
+
 }
